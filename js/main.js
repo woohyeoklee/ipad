39301a8331317adf3b6c715861dca638811529db
woohyeoklee <whyeok94@gmail.com>
2024-01-30 16:27:32 +0900

    observe

--- js/main.js
+++ js/main.js
@@ -65,3 +65,19 @@ function hideSearch() {
   searchDelayEls.reverse()
   searchInputEl.value = ''
 }
+
+// 요소의 가시성 관찰 로직!
+const io = new IntersectionObserver(entries => {
+  // entries는 `io.observe(el)`로 등록된 모든 관찰 대상 배열.
+  entries.forEach(entry => {
+    // 사라질 때.
+    if (!entry.isIntersecting) {
+      return
+    }
+    entry.target.classList.add('show')
+  })
+})
+// 관찰할 요소들 검색
+const infoEls = document.querySelectorAll('.info')
+// 관찰 시작!
+infoEls.forEach(el => io.observe(el))
